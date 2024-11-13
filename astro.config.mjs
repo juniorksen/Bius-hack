ac0f78905b655a39e567aecac86c4746ce69fe8e
alfonso <alfonsohernandezrincon4@gmail.com>
2024-11-13 16:28:45 -0500

    Configurado adaptador de Node.js y ajustes de construcción

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import node from '@astrojs/node'; // Importa el adaptador de Node.js
 
 export default defineConfig({
     integrations: [
         tailwind(),
         react(),
     ],
-    output: 'server',
+    output: 'server', // Establece el tipo de salida como servidor
+    adapter: node({ mode: 'standalone' }), // Configura el adaptador de Node.js con el modo 'standalone'
     server: {
-        port: 3000,
+        port: 3000, // Configura el puerto del servidor
     },
 });
